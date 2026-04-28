c4f4d79bfe6fb705db0881e270549cc401fc886f
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-04-28 12:55:36 +0530

    added typed function example

---
+++ functions/typed-functions.ts
@@ -0,0 +1,8 @@
+// Typed Functions in TypeScript
+
+function typedExample(a: number, b: string): any {
+  return a + b;
+}
+console.log("typedFn result", typedExample(5, "hello"));
+
+// 2. Function returning void
