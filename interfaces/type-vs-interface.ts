3d41f3864418cab50a5d4d5122bbc2527a7126c7
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-05-03 16:35:49 +0530

    extension vs intersection added

--- interfaces/type-vs-interface.ts
+++ interfaces/type-vs-interface.ts
@@ -18,3 +18,36 @@ interface A {
 interface B extends A {
   age: number;
 }
+
+// Type
+type x = {
+  name: string;
+};
+
+type y = x & {
+  age: number;
+};
+
+// 3️⃣ Union (ONLY type)
+// type ID = string | number;
+// 4️⃣ Declaration merging (ONLY interface)
+// interface User {
+//   name: string;
+// }
+
+// interface User {
+//   age: number;
+// }
+
+// const user: User = {
+//   name: "Praveena",
+//   age: 24
+// };
+// 5️⃣ Function type (both ways)
+// // Type
+// type Add = (a: number, b: number) => number;
+
+// // Interface
+// interface AddFn {
+//   (a: number, b: number): number;
+// }
